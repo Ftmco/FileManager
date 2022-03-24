739be5e0fb7b76c493cdb37f7760db86cc1192bc
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-24 13:05:02 -0700

    create db context

---
+++ FileCloud.Server/File.DataBase/Context/FileContext.cs
@@ -0,0 +1,31 @@
+﻿using File.Entity.Application;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File.DataBase.Context;
+
+public class FileContext : DbContext
+{
+    public static string ConnectionString { get; set; }
+
+    public FileContext(string connectionString)
+    {
+        if (!string.IsNullOrEmpty(connectionString))
+            ConnectionString = connectionString;
+    }
+
+    public FileContext(DbContextOptions<FileContext> options) : base(options)
+    {
+
+    }
+
+    public virtual DbSet<Application> Application { get; set; }
+
+    public virtual DbSet<Entity.File.Directory> Directory { get; set; }
+
+    public virtual DbSet<Entity.File.File> File { get; set; }
+}
