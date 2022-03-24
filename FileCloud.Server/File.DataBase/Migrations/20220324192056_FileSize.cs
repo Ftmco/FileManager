1bb2d9171ffaf3d644bf64562c899fc4b40fb1bd
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-24 13:05:56 -0700

    update data base

---
+++ FileCloud.Server/File.DataBase/Migrations/20220324192056_FileSize.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace File.DataBase.Migrations
+{
+    public partial class FileSize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Size",
+                table: "File",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "File");
+        }
+    }
+}
