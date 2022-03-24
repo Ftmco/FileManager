c63e471621f22f2c699285c73dccd97c4be15eb0
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-24 13:06:14 -0700

    application entities

---
+++ FileCloud.Server/File.Entity/Application/Application.cs
@@ -0,0 +1,24 @@
+﻿namespace File.Entity.Application;
+
+public record Application
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string ApiKey { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
+    [Required]
+    public Guid OwnerId { get; set; }
+
+    //Navigation Property
+    //Relationships
+
+    public virtual ICollection<File.Directory> Directories { get; set; }
+}
