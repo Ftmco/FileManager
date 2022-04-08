d3a01fee9ac5b232c8b80b21b83ba148568a0790
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-04-08 12:15:27 -0700

    inject file services

--- FileCloud.Server/File.Implemention/Injector/Injector.cs
+++ FileCloud.Server/File.Implemention/Injector/Injector.cs
@@ -1,4 +1,7 @@
 ﻿using File.DataBase.Context;
+using File.Implemention.Base;
+using Identity.Client.Rules;
+using Identity.Client.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -23,20 +26,24 @@ public static class Injector
 
     public static Task<IServiceCollection> AddBaseQueryAsync(this IServiceCollection services)
     {
-      
+        services.AddScoped<IBaseQuery<FDirectory, FileContext>, BaseQuery<FDirectory, FileContext>>();
         return Task.FromResult(services);
     }
 
     public static Task<IServiceCollection> AddBaseCudAsync(this IServiceCollection services)
     {
-       
+        services.AddScoped<IBaseCud<FDirectory, FileContext>, BaseCud<FDirectory, FileContext>>();
         return Task.FromResult(services);
     }
 
     public static Task<IServiceCollection> AddServicesAsync(this IServiceCollection services)
     {
+        services.AddTransient<IDirectoryAction, DirectoryAction>();
+        services.AddTransient<IDirectoryViewModel, DirectoryViewModel>();
         return Task.FromResult(services);
     }
 
-    public static Task<IServiceCollection> AddToolsAsync(this IServiceCollection services) { return Task.FromResult(services); }
+    public static Task<IServiceCollection> AddToolsAsync(this IServiceCollection services) {
+        services.AddTransient<IAccountRules, AccountService>();
+        return Task.FromResult(services); }
 }
