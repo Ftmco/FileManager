01bfee6918e6425c84e82c9ab7f6924cfc26d4ce
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-04-08 12:16:21 -0700

    Create Directory.cs

---
+++ FileCloud.Server/File.ViewModel/Directory/Directory.cs
@@ -0,0 +1,15 @@
+﻿namespace File.ViewModel;
+
+public record DirectoryViewModel(Guid Id, Guid? ParentId, string Name,string Token, DateTime CreateDate, DateTime LastUpdate);
+
+public record UpsertDirectory(Guid? ParentId, Guid? Id, string Name);
+
+public record UpsertDirectoryResponse(DirecttoryActionStatus Status, DirectoryViewModel? Directory);
+
+public enum DirecttoryActionStatus
+{
+    Success = 0,
+    AccessDenied = 1,
+    Exception = 2,
+    ObjectNotFound = 3
+}
