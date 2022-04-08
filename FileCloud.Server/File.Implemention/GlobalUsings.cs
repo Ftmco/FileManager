4c16f7a503efb79782a6edc921788c9fd9ae2fbe
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-04-08 12:16:35 -0700

    Update GlobalUsings.cs

--- FileCloud.Server/File.Implemention/GlobalUsings.cs
+++ FileCloud.Server/File.Implemention/GlobalUsings.cs
@@ -1,3 +1,6 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using System.Linq.Expressions;
 global using File.Abstraction.Base;
+global using File.Abstraction;
+global using File.Entity;
+global using File.DataBase.Context;
