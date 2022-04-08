b3c1725c65229bd979345336bbebff88492b7b6d
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-04-08 12:16:13 -0700

    Create Token Services

---
+++ FileCloud.Server/File.Extension/Code/Token.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File.Extension.Code;
+
+public static class TokenExtension
+{
+    public static string CreateToken(this int length)
+    {
+        string newStr = Guid.NewGuid().ToString().Replace("-", "");
+        while (newStr.Length < length)
+            newStr += Guid.NewGuid().ToString().Replace("-", "");
+        return newStr;
+    }
+}
