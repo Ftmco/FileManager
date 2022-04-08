d8660a8a9f6e01dc9f749ff070ab04e387c27826
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-04-08 12:14:54 -0700

    file view models

---
+++ FileCloud.Server/File.ViewModel/File/File.cs
@@ -0,0 +1,18 @@
+﻿namespace File.ViewModel;
+
+public record FileViewModel(Guid Id, string Name, DateTime CreateDate, string Directory, string DireToken, string Token);
+
+public record CreateFile(string DirectoryToken, string Base64, string Name, string Mime);
+
+public record CreateFileResponse(FileActionStatus Status, FileViewModel? File);
+
+public enum FileActionStatus
+{
+    Success = 0,
+    AccessDenied = 1,
+    Exception = 2,
+    NotFound = 3,
+    DirectoryNotFound = 4,
+}
+
+public record SaveFile(string Base64,string Path,string Name);
