113626396b472e75e03c06550f6e22f445d3a2c9
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-04-14 06:14:16 -0700

    create delete file servicer

--- FileCloud.Server/File.Abestraction/File/IFileAction.cs
+++ FileCloud.Server/File.Abestraction/File/IFileAction.cs
@@ -4,5 +4,13 @@ public interface IFileAction : IAsyncDisposable
 {
     Task<CreateFileResponse> CreateFileAsync(CreateFile file);
 
-    Task<bool> SaveFileAsync(SaveFile saveFile);
+    Task DeleteFileAsync(Guid id);
+
+    Task DeleteFileAsync(string token);
+
+    Task DeleteFileAsync(DirectoryFile file);
+
+    Task<bool> SaveFileOnDiskAsync(SaveFile saveFile);
+
+    Task<bool> DeleteFileFromDiskAsync(string path);
 }
--- FileCloud.Server/File.Implemention/File/FileAction.cs
+++ FileCloud.Server/File.Implemention/File/FileAction.cs
@@ -1,6 +1,9 @@
 ﻿using File.Extension.Code;
 using File.ViewModel;
 using Microsoft.Extensions.Configuration;
+using System.Text.RegularExpressions;
+using static System.IO.File;
+using static System.Convert;
 
 namespace File.Implemention;
 
@@ -10,21 +13,25 @@ public class FileAction : IFileAction
 
     readonly IBaseCud<DirectoryFile, FileContext> _fileCud;
 
+    readonly IBaseQuery<DirectoryFile, FileContext> _fileQuery;
+
     readonly IFileViewModel _fileViewModel;
 
     readonly IConfiguration _configuration;
 
-    public FileAction(IDirectoryGet directoryGet, IBaseCud<DirectoryFile, FileContext> fileCud, IFileViewModel fileViewModel, IConfiguration configuration)
+    public FileAction(IDirectoryGet directoryGet, IBaseCud<DirectoryFile, FileContext> fileCud, IFileViewModel fileViewModel,
+        IConfiguration configuration, IBaseQuery<DirectoryFile, FileContext> fileQuery)
     {
         _directoryGet = directoryGet;
         _fileCud = fileCud;
         _fileViewModel = fileViewModel;
         _configuration = configuration;
+        _fileQuery = fileQuery;
     }
 
     public async Task<CreateFileResponse> CreateFileAsync(CreateFile file)
     {
-        var dir = await _directoryGet.GetDirectoryAsync(file.DirectoryToken);
+        FDirectory? dir = await _directoryGet.GetDirectoryAsync(file.DirectoryToken);
         if (dir != null)
         {
             DirectoryFile directoryFile = new()
@@ -37,14 +44,57 @@ public class FileAction : IFileAction
                 Name = $"file_{20.CreateToken()}.{file.Mime.Split("/")[1]}",
                 Token = $"file_{20.CreateToken()}",
             };
-            if (await _fileCud.InsertAsync(directoryFile))
+            if (await SaveFileOnDiskAsync(new(file.Base64, dir.Path, directoryFile.Name)))
             {
-                await SaveFileAsync(new(file.Base64, dir.Path, directoryFile.Name));
-                return new CreateFileResponse(FileActionStatus.Success, await _fileViewModel.CreateFileViewModel(directoryFile));
+                if (await _fileCud.InsertAsync(directoryFile))
+                    return new CreateFileResponse(FileActionStatus.Success, await _fileViewModel.CreateFileViewModel(directoryFile));
+                else
+                {
+                    await DeleteFileFromDiskAsync($"{dir}/{directoryFile.Name}");
+                    return new CreateFileResponse(FileActionStatus.Exception, null);
+                }
             }
             return new CreateFileResponse(FileActionStatus.Exception, null);
         }
         return new CreateFileResponse(FileActionStatus.DirectoryNotFound, null);
+    }
+
+    public async Task DeleteFileAsync(Guid id)
+    {
+        var file = await _fileQuery.GetAsync(id);
+        if (file != null)
+            await DeleteFileAsync(file);
+    }
+
+    public async Task DeleteFileAsync(string token)
+    {
+        var file = await _fileQuery.GetAsync(f=> f.Token == token);
+        if (file != null)
+            await DeleteFileAsync(file);
+    }
+
+    public async Task DeleteFileAsync(DirectoryFile file)
+    {
+        var dir = await _directoryGet.GetDirectoryAsync(file.DirectoryId);
+        if(dir != null)
+        {
+            string path = $"{dir.Path}/{file.Name}";
+            await DeleteFileFromDiskAsync(path);
+        }
+    }
+
+    public Task<bool> DeleteFileFromDiskAsync(string path)
+    {
+        try
+        {
+            if (Exists(path))
+                Delete(path);
+            return Task.FromResult(true);
+        }
+        catch
+        {
+            return Task.FromResult(false);
+        }
     }
 
     public async ValueTask DisposeAsync()
@@ -53,14 +103,15 @@ public class FileAction : IFileAction
         await _directoryGet.DisposeAsync();
     }
 
-    public async Task<bool> SaveFileAsync(SaveFile saveFile)
+    public async Task<bool> SaveFileOnDiskAsync(SaveFile saveFile)
     {
         try
         {
+            string[] base64Split = saveFile.Base64.Split(",");
             string rootPath = _configuration["BaseDirectories:BaseDir"];
             string path = rootPath + saveFile.Path + $"/{saveFile.Name}";
-            byte[]? bytes = Convert.FromBase64String(saveFile.Base64);
-            await System.IO.File.WriteAllBytesAsync(path, bytes);
+            byte[]? bytes = FromBase64String(base64Split.Last());
+            await WriteAllBytesAsync(path, bytes);
             return true;
         }
         catch
